23ac1c8301acff6a3b5b0d7bb172d927171f4fed
Lihini Gamage <126759357+LbGamage@users.noreply.github.com>
2025-04-11 17:20:57 +1000

    got the cursor pencil working

--- Week 4 P5/sketch.js
+++ Week 4 P5/sketch.js
@@ -16,10 +16,17 @@ function draw() {
   // Copy a small portion from topImg to the canvas as you drag
   // copy(topImg, mouseX, mouseY, 10, 10, mouseX, mouseY, 10, 10);
 
-  stroke(mouseX, mouseY, 150);
-line(mouseX, mouseY, pmouseX, pmouseY);
-}
+  if (mouseIsPressed) {
+    let x = map(mouseX, 0, width, 0, 255);
+    let y = map(mouseY, 0, width, 0, 255);
+    stroke(x, y, 150);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+
+;}
 
 function keyPressed() {
-  background(0); // Clear the canvas
+  if (key === ' ') {
+     background(0); // Clear the canvas
+  }
 }
