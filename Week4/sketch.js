f53244912dc7eedb832c98c5e553b477e478599e
Lihini Gamage <126759357+LbGamage@users.noreply.github.com>
2025-04-18 04:52:48 +1000

    8 ball slave to the algorithm webpage done

--- Week4/sketch.js
+++ Week4/sketch.js
@@ -31,7 +31,7 @@ function setup() {
   // Create clickable images for each preloaded image. Co-pilot helped to edit this function so it applies to all images.
   createClickableImage('images/heart-doodle.png', 'clickable heart', 'self-potraint.html', 150, 150);
   createClickableImage('images/smile-doodle.png', 'clickable smile', 'self-potraint.html', 150, 150);
-  createClickableImage('images/spiral doodle.png', 'clickable spiral', 'spiral-page.html', 150, 150);
+  createClickableImage('images/spiral doodle.png', 'clickable spiral', '8ball.html', 150, 150);
   createClickableImage('images/flower-doodle.png', 'clickable flower', 'kusama.html', 150, 150);
 
   let titleImg = createImg('images/week-4 header.png', 'title');
--- Week4/slavealgo.js
+++ Week4/slavealgo.js
@@ -1,9 +1,40 @@
+var my8Ball; // variable to store 8 ball image
+
+// variable to store the fortune that is picked
+var myFortune = "";
+
+// the list of possible fortunes
+var myFortuneArray = ["Yes!",
+  "No",
+  "Maybe so",
+  "Ask again",
+  "Absolutely!"
+];
+
+function preload() {
+  my8Ball = loadImage("images/8ball.png");
+}
+
+
 function setup() {
-    createCanvas(windowWidth, windowHeight, WEBGL);
-    background(0);
-    angleMode(DEGREES);
-  }
-  
-  function draw() {
-    
-  }
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+}
+
+function draw() {
+  background(0);
+  imageMode(CENTER);
+  image(my8Ball, windowWidth/2, windowHeight/2, windowHeight, windowHeight);
+  //display instructions
+  textAlign(CENTER);
+  textSize(windowHeight / 22);
+  fill(0);
+  text(myFortune, windowWidth/1.9, windowHeight/2.1);
+
+}
+
+// when mouse is pressed, set my fortune to a random choice from my fortune list and make the ellipse fully opaque
+
+function mousePressed() {
+  myFortune = random(myFortuneArray);
+}
