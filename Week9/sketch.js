3aa702eb53f35a20a91f02bdb4ea9faab980fe57
Lihini Gamage <126759357+LbGamage@users.noreply.github.com>
2025-05-16 14:35:30 +1000

    final touches

---
+++ Week9/sketch.js
@@ -0,0 +1,14 @@
+let noiseScale = .01
+
+function setup(){
+  background(0);
+  createCanvas(windowWidth,windowHeight)
+}
+
+function draw(){
+for (let x=0; x<width; x++){
+  let noiseVal = noise((mouseX+x)*noiseScale, mouseY*noiseScale);
+  stroke(noiseVal*255,random(0,255),255);
+  line(x,mouseY+noiseVal*80,x,height);
+}
+}
